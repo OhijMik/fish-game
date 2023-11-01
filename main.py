910ea8d272ffa7fd8ea153d5715e140e40c671ab
OhijMik <jihok148@gmail.com>
2023-11-01 10:52:39 -0400

    Created my own branch

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+
+pygame.init()
+
+# The main function
+def main():
+    pygame.init()      # Prepare the pygame module for use
+    surfaceSize = [800, 600]   # Desired physical surface size, in pixels.
+    
+    clock = pygame.time.Clock()  #Force frame rate to be slower
+    frameRate = 60               #Slowing down the program
+    frameCount = 0               #Count the number of frames that have occurred
+    
+    # Create surface of (width, height), and its window.
+    mainSurface = pygame.display.set_mode((surfaceSize[0], surfaceSize[1]))
+
+
+    while True:
+        ev = pygame.event.poll()   
+        if ev.type == pygame.QUIT:  
+            break
+
+        
+
+
+    pygame.display.flip()
+    
+    frameCount += 1
+    clock.tick(frameRate) #Force frame rate to be slower
+
+    pygame.quit()     # Once we leave the loop, close the window.
+# Running the main loop
+main()
